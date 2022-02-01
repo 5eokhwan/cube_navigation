edd42285a9b2096364d745b10a6f5c62fec2891b
exist909@naver.com <exist909@naver.com>
2022-02-01 20:56:16 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [isActive, setIsActive] = useState(false);
+  //회전율 0이면 정면을 향하는 것으로 한다.
+  const [currentRotate, setCurrentRetrotate] = useState(0);
+  useEffect(() => {
+    window.onmousemove = (e) => {
+      console.log(
+        "rotateX(" +
+          (e.clientY % 360) +
+          "deg) rotateY(" +
+          (e.clientX % 360) +
+          "deg)"
+      );
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div id="CubeScene" className={isActive ? "active" : ""}>
+        <div
+          className="cube"
+          onClick={() => {
+            setIsActive(!isActive);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div className="face top"></div>
+          <div className="face left">left</div>
+          <div className="face front">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className="face right">right</div>
+          <div className="face back">back</div>
+          <div className="face bottom">bottom</div>
+        </div>
+      </div>
+      <div id="MenuDesc">Menu</div>
+    </>
   );
 }
 
