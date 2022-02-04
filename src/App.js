4781ec26bb3d99cd3ceb7496913f708c474b414b
exist909@naver.com <exist909@naver.com>
2022-02-04 18:24:59 +0900

    active시 회전 구전

--- src/App.js
+++ src/App.js
@@ -1,31 +1,64 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 function App() {
+  function updatePos(e) {
+    //마우스 이벤트 발생시 마우스 갱신
+    if (e) pos.current.mouse = [e.clientX, e.clientY];
+    pos.current.cube = [
+      center.current.getBoundingClientRect().left,
+      center.current.getBoundingClientRect().top,
+    ];
+    currentRotate.current = [
+      pos.current.mouse[0] - pos.current.cube[0],
+      pos.current.mouse[1] - pos.current.cube[1],
+    ];
+    //console.log("mousePos:", pos.current.mouse, "mouseCube:", pos.current.cube);
+  }
   const [isActive, setIsActive] = useState(false);
   //회전율 0이면 정면을 향하는 것으로 한다.
-  const [currentRotate, setCurrentRetrotate] = useState(0);
+  const currentRotate = useRef([]);
+  const cubeNavigation = useRef();
+  const center = useRef();
+
+  const pos = useRef({
+    mouse: [0, 0],
+    cube: [0, 0],
+  });
+
   useEffect(() => {
-    window.onmousemove = (e) => {
-      console.log(
-        "rotateX(" +
-          (e.clientY % 360) +
+    console.log(pos.current.mouse);
+    let mouseMoveInterval;
+    if (isActive) {
+      mouseMoveInterval = setInterval(() => {
+        updatePos();
+        cubeNavigation.current.style.transform =
+          "rotateX(" +
+          (pos.current.mouse[1] - pos.current.cube[1]) +
           "deg) rotateY(" +
-          (e.clientX % 360) +
-          "deg)"
-      );
+          (pos.current.mouse[0] - pos.current.cube[0]) +
+          "deg)";
+      }, 15);
+      window.onclick = updatePos;
+      window.onmousemove = updatePos;
+    } else {
+    }
+    return () => {
+      clearInterval(mouseMoveInterval);
     };
-  }, []);
+  }, [isActive]);
 
   return (
     <>
       <div id="CubeScene" className={isActive ? "active" : ""}>
-        <div
+        <nav
           className="cube"
           onClick={() => {
             setIsActive(!isActive);
           }}
+          ref={cubeNavigation}
         >
+          <div className="center" ref={center}></div>
           <div className="face top"></div>
           <div className="face left">left</div>
           <div className="face front">
@@ -36,7 +69,7 @@ function App() {
           <div className="face right">right</div>
           <div className="face back">back</div>
           <div className="face bottom">bottom</div>
-        </div>
+        </nav>
       </div>
       <div id="MenuDesc">Menu</div>
     </>
