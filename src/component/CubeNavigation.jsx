ce361ce6e33b8c6119011c86522ddf370425522b
exist909@naver.com <exist909@naver.com>
2022-02-06 03:09:46 +0900

    포커스 선택

--- src/component/CubeNavigation.jsx
+++ src/component/CubeNavigation.jsx
@@ -3,7 +3,14 @@ import Face from "./Face";
 import { useState, useEffect, useRef } from "react";
 
 const sides = ["front", "left", "back", "right", "top", "bottom"];
-const iconNames = ["logo-dropbox", "log-in", "radio-button-on", "map", "grid"];
+const iconNames = [
+  null,
+  "logo-dropbox",
+  "log-in",
+  "radio-button-on",
+  "map",
+  "grid",
+];
 
 function returnCurFace(x, y) {
   let nx = (x % 360) / 90;
@@ -33,14 +40,11 @@ function CubeNavigation() {
       pos.current.mouse[0] - pos.current.cube[0],
       pos.current.mouse[1] - pos.current.cube[1],
     ];
-    console.log(currentRotate.current);
-    //console.log("mousePos:", pos.current.mouse, "mouseCube:", pos.current.cube);
   }
 
   const [isActive, setIsActive] = useState(false);
   //회전율 0이면 정면을 향하는 것으로 한다.
   const currentRotate = useRef([]);
-  const currentFocus = useRef("front");
   const [focus, setFocus] = useState("front");
   const cubeNavigation = useRef();
   const center = useRef();
@@ -56,10 +60,6 @@ function CubeNavigation() {
       cubeNavigation.current.style.transition = "";
       mouseMoveInterval = setInterval(() => {
         updatePos();
-        // currentFocus.current = returnCurFace(
-        //   currentRotate.current[0],
-        //   currentRotate.current[1]
-        // );
         setFocus(
           returnCurFace(currentRotate.current[0], currentRotate.current[1])
         );
@@ -70,28 +70,30 @@ function CubeNavigation() {
           currentRotate.current[0] +
           "deg)";
       }, 15);
-      window.onmousemove = updatePos;
-    } else {
     }
     window.onclick = updatePos;
     return () => {
       //인라인 스타일을 제거하여 css 스타일을 적용되게 한다
       if (isActive) {
+        cubeNavigation.current.style.transform = "";
         cubeNavigation.current.style.transition =
           "width 0.75s, height 0.75s, transform 0.75s";
-        cubeNavigation.current.style.transform = "";
         clearInterval(mouseMoveInterval);
-      }
+      } else window.onmousemove = updatePos;
     };
   }, [isActive]);
-
+  const updateCurMenu = () => {
+    console.log(focus);
+    setIsActive(false);
+  };
   return (
     <>
+      {isActive && <div id="TouchScreen" onClick={updateCurMenu}></div>}
       <div id="CubeScene" className={isActive ? "active" : ""}>
         <nav
           className="cube"
           onClick={() => {
-            setIsActive(!isActive);
+            if (!isActive) setIsActive(true);
           }}
           ref={cubeNavigation}
         >
@@ -99,15 +101,16 @@ function CubeNavigation() {
           {sides.map((v, i) => {
             return (
               <Face
+                key={v + i}
                 side={v}
-                name={v !== "front" ? iconNames[i] : null}
+                name={iconNames[i]}
                 isActive={focus === v ? true : false}
               />
             );
           })}
         </nav>
+        {isActive && <div id="MenuDesc">{focus}</div>}
       </div>
-      <div id="MenuDesc">Menu</div>
     </>
   );
 }
