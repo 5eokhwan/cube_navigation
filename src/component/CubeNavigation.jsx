95634e0677fb5e0ecb020e1599e593e8b8c73d7b
exist909@naver.com <exist909@naver.com>
2022-02-05 16:56:45 +0900

    포커스 active효과

--- src/component/CubeNavigation.jsx
+++ src/component/CubeNavigation.jsx
@@ -1,4 +1,5 @@
 import "./CubeNavigation.scss";
+import Face from "./Face";
 import { useState, useEffect, useRef } from "react";
 
 function CubeNavigation() {
@@ -16,45 +17,47 @@ function CubeNavigation() {
     console.log(currentRotate.current);
     //console.log("mousePos:", pos.current.mouse, "mouseCube:", pos.current.cube);
   }
+  function returnCurFace(x, y) {
+    let nx = (x % 360) / 90;
+    const ny = (y % 360) / 90;
+    if ((2.5 <= ny && ny < 3.5) || (-1.5 <= ny && ny < -0.5)) return "top";
+    else if ((-3.5 <= ny && ny < -2.5) || (0.5 <= ny && ny < 1.5))
+      return "bottom";
+
+    if ((1.5 <= ny && ny < 2.5) || (-2.5 <= ny && ny < -1.5)) {
+      return sides[Math.round(nx + 2) % 4];
+    } else {
+      return sides[Math.round(nx) % 4];
+    }
+  }
 
   const [isActive, setIsActive] = useState(false);
   //회전율 0이면 정면을 향하는 것으로 한다.
   const currentRotate = useRef([]);
   const currentFocus = useRef("front");
-  // / 90
-  const forcusMap = useRef({
-    front: [
-      [0, 0],
-      [2, 2],
-    ],
-    left: [
-      [1, 0],
-      [2, 3],
-    ],
-    back: [
-      [2, 0],
-      [0, 2],
-    ],
-    right: [3, 0],
-    top: [0, 3],
-    bottom: [0, 1],
-  });
-
+  const [focus, setFocus] = useState("front");
   const cubeNavigation = useRef();
   const center = useRef();
-
+  const sides = ["front", "left", "back", "right", "top", "bottom"];
   const pos = useRef({
     mouse: [0, 0],
     cube: [0, 0],
   });
 
   useEffect(() => {
-    console.log(pos.current.mouse);
+    // console.log(pos.current.mouse);
     let mouseMoveInterval;
     if (isActive) {
       cubeNavigation.current.style.transition = "";
       mouseMoveInterval = setInterval(() => {
         updatePos();
+        // currentFocus.current = returnCurFace(
+        //   currentRotate.current[0],
+        //   currentRotate.current[1]
+        // );
+        setFocus(
+          returnCurFace(currentRotate.current[0], currentRotate.current[1])
+        );
         cubeNavigation.current.style.transform =
           "rotateX(" +
           currentRotate.current[1] +
@@ -88,7 +91,7 @@ function CubeNavigation() {
           ref={cubeNavigation}
         >
           <div className="center" ref={center}></div>
-          <div className="face top">
+          {/* <div className="face top active">
             <ion-icon
               name="logo-dropbox"
               style={{ width: "75%", height: "75%" }}
@@ -99,13 +102,22 @@ function CubeNavigation() {
               name="log-in"
               style={{ width: "75%", height: "75%" }}
             ></ion-icon>
-          </div>
-          <div className="face front">
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-          <div className="face right">
+          </div> */}
+
+          {sides.map((v) => {
+            if (v === "front")
+              return (
+                <div className="face front">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </div>
+              );
+            return <Face side={v} isActive={focus === v ? true : false} />;
+          })}
+
+          {/* <div className="face right">
+            right
             <ion-icon
               name="radio-button-on"
               style={{ width: "75%", height: "75%" }}
@@ -118,11 +130,12 @@ function CubeNavigation() {
             ></ion-icon>
           </div>
           <div className="face bottom">
+            bottom
             <ion-icon
               name="grid"
               style={{ width: "75%", height: "75%" }}
             ></ion-icon>
-          </div>
+          </div> */}
         </nav>
       </div>
       <div id="MenuDesc">Menu</div>
@@ -131,3 +144,4 @@ function CubeNavigation() {
 }
 
 export default CubeNavigation;
+//face를 컴포넌트화 하여 active클래스에 집어넣는다.
--- src/component/Face.jsx
+++ src/component/Face.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-function face() {
-  return <div></div>;
+function face({ isActive, side, name }) {
+  return (
+    <div className={isActive ? `face ${side} active` : `face ${side}`}>
+      <ion-icon name={name} style={{ width: "75%", height: "75%" }}></ion-icon>
+    </div>
+  );
 }
 
 export default face;
